361660c04c10fb607aaa3f0890c82ae3d02ff9ad
Nolan Fuchs <nolan1235@gmail.com>
2018-11-13 10:38:56 -0700

    Initial Commit

---
+++ iOS Music/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Music
+//
+//  Created by Nolan Fuchs on 11/13/18.
+//  Copyright © 2018 Nolan Fuchs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
